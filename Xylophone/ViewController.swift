0877e6572699f51548d89a8717c32c03dbf9b85a
Niko Berdzenishvili <96183439+NikoBerd@users.noreply.github.com>
2022-09-16 22:08:11 +0400

    Update ViewController.swift

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,32 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  
+  var player: AVAudioPlayer!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  @IBAction func keyPressed(_ sender: UIButton) {
+    /* sender.currentTitle - is that UIView that or "button" that gets pressed.
+     */
+    playSound(soundName: sender.currentTitle!)
+    //changing UIView alpha setting to 0.5 when it's pressed
+    sender.alpha = 0.5
+    // a smooth transition + getting original alpha = 1.0 back
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.15 ) {
+      UIView.animate(withDuration: 0.15) {
+        sender.alpha = 1.0
+      }
     }
-
-
     
-
+    func playSound(soundName: String) {
+      let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+      player = try! AVAudioPlayer(contentsOf: url!)
+      player.play()
+      
+    }
+  }
 }
-
